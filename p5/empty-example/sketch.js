697fdefdb7eb2c537c5c6b8add2f79942d9f4834
Aggie Deberny <a@deberny.com>
2016-06-17 10:49:56 +0100

    fixed mouse click grabs the right colour value

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -3,6 +3,8 @@ var w;
 var h;
 var osc;
 var fft;
+var frameWidth=640;
+var frameHeight=480;
 
 function setup() {
   // w = 800;
@@ -12,11 +14,13 @@ function setup() {
     h = w * 3 / 4;               // 4:3 camera aspect?
     myCanvas = createCanvas(w, h);
     myCanvas.parent('myContainer');
-    stroke(0);
+    offsetX = (w-frameWidth)/2;
+    offsetY = (h-frameHeight)/2;
 
     capture = createCapture(VIDEO);
-    capture.size(w, h);
+    capture.size(frameWidth, frameHeight);
     capture.hide();             // Stop the raw capture appearing too.
+    frameRate(10);
 
     // osc = new p5.TriOsc(); // set frequency and type
     osc = new p5.Oscillator();
@@ -32,35 +36,69 @@ function draw() {
     background(0);
     var d = pixelDensity();
 
-  imageMode(CENTER);
-  image(capture, w/2, h/2, w, h);
-  //filter(GRAY);
+    image(capture, offsetX, offsetY);
+    //filter(GRAY);
 
-  capture.loadPixels();
-  capture.get();
-  var index = 0;
-  index = capture.pixels[Math.floor(mouseX + (mouseY * w/2 * 4)) * d];
-  console.log(Math.floor(mouseX + (mouseY * w * 4)) * d);
-  console.log(index);
+    // capture.loadPixels();
 
-  // change oscillator frequency based on index value
-  if (index !== undefined) {
-    var freq = map(index, 0, 255, 40, 880);
-    osc.freq(freq);
+    // var yomo = capture.get(mouseX,mouseY,10,10);
+    // console.log(yomo);
 
-    var amp = map(index, 0, 255, 1, .01);
-    osc.amp(amp);
-  } else {
-    console.log("holly shit")
-  }
+
+    var index = 0;
+
+    // var c = capture.get(50,90);
+    // console.log(c);
+
+  // index = capture.pixels[Math.floor(mouseX + (mouseY * w/2 * 4)) * d];
+  // console.log(Math.floor(mouseX + (mouseY * w * 4)) * d);
+  // console.log(index);
+
+  // // change oscillator frequency based on index value
+  //   if (index !== undefined) {
+  //     var freq = map(index, 0, 255, 40, 880);
+  //     osc.freq(freq);
+
+  //     var amp = map(index, 0, 255, 1, .01);
+  //     osc.amp(amp);
+  // } else {
+  //   console.log("holly shit");
+  // }
 }
 
 function mousePressed() {
+
+  capture.loadPixels();
+
+  var c = capture.get(mouseX-offsetX,mouseY-offsetY);
+  
+  console.log(c);
+
+  // console.log('kurfa');
+
+  // capture.loadPixels();
+
+  // var x = mouseX;
+
+  // var y = mouseY;
+
+
+  // var d = pixelDensity();
+
+  // var off = ( x + (y * w/2 * 4)) * d; 
+
+  // var grabd = [pixels[off], pixels[off+1], pixels[off+2], pixels[off+3]]; 
+
+  // console.log(grabd);
+
+  
   if ( song.isPlaying() ) { // .isPlaying() returns a boolean
     song.stop();
+
     //background(255,0,0);
   } else {
     song.play();
     //background(0,255,0);
+
   }
 }
